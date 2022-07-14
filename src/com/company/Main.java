d041755b1537d069d6aade812f9b3d9ec7fe27fa
ASUS <Azamatkal87@gmail.com>
2022-07-14 23:14:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class clas = new Class(5,"Salam",20);
+        System.out.println(clas.getNomer());
+        System.out.println(clas.getSoz());
+        System.out.println(clas.getMassive());
+
+
+    }
+}
